be8372c8a65e8f1f03d2941c67e5ba4e7c2be0d1
Full Queue Developer <FullQueueDeveloper@FullQueueDeveloper.com>
2023-06-08 11:51:12 -0400

    detect public as an extension on DeclGroupSyntax

--- Sources/IdentifiedEnumCasesMacro/IdentifiedEnumCasesMacro.swift
+++ Sources/IdentifiedEnumCasesMacro/IdentifiedEnumCasesMacro.swift
@@ -11,8 +11,6 @@ public struct IdentifiedEnumCasesMacro: MemberMacro {
     providingMembersOf declaration: Declaration,
     in context: Context) throws -> [SwiftSyntax.DeclSyntax] where Declaration : SwiftSyntax.DeclGroupSyntax, Context : SwiftSyntaxMacros.MacroExpansionContext {
       
-      
-      
       guard let declaration = declaration.as(EnumDeclSyntax.self) else {
         let enumError = Diagnostic(node: node._syntaxNode, message: Diagnostics.mustBeEnum)
         context.diagnose(enumError)
@@ -20,54 +18,14 @@ public struct IdentifiedEnumCasesMacro: MemberMacro {
       }
       
       
-      print("-------------------")
-      let keywords: [Keyword] = {
-        if let modifiers = declaration.modifiers {
-          return modifiers.children(viewMode: .fixedUp).flatMap { syntax in
-            return syntax.as(DeclModifierSyntax.self)?.children(viewMode: .fixedUp).compactMap({ syntax in
-              switch syntax.as(TokenSyntax.self)?.tokenKind {
-              case .keyword(.public):
-                return Keyword.public
-                
-              default:
-                return nil
-              }
-            }) ?? []
-          }
-        } else {
-          return []
-        }
-      }()
-      print("```````")
-      print(keywords)
-      print("```````")
-//      let keywords: [Keyword] = declaration.modifiers?.children(viewMode: .fixedUp).map({ syntax in
-//        syntax.as(DeclModifierSyntax.self)?.children(viewMode: .fixedUp).compactMmap({ syntax in
-//          switch syntax.as(TokenSyntax.self)?.tokenKind {
-//          case .keyword(.public):
-//            return Keyword.public
-//            
-//          default:
-//            return nil
-//          }
-//                 
-//        })
-//      })
-
-
-      print(declaration.identifier)
-
       guard let enumCases: [SyntaxProtocol] = declaration.memberBlock
-        .children(viewMode: .fixedUp)
-        .filter({ $0.kind == .memberDeclList })
+        .children(viewMode: .fixedUp).filter({ $0.kind == .memberDeclList })
         .first?
-        .children(viewMode: .fixedUp)
-        .filter({ $0.kind == SyntaxKind.memberDeclListItem })
+        .children(viewMode: .fixedUp).filter({ $0.kind == SyntaxKind.memberDeclListItem })
         .flatMap({ $0.children(viewMode: .fixedUp).filter({ $0.kind == .enumCaseDecl })})
         .flatMap({ $0.children(viewMode: .fixedUp).filter({ $0.kind == .enumCaseElementList })})
         .flatMap({ $0.children(viewMode: .fixedUp).filter({ $0.kind == .enumCaseElement })})
       else {
-        
         let enumError = Diagnostic(node: node._syntaxNode, message: Diagnostics.mustHaveCases)
         context.diagnose(enumError)
         return []
@@ -94,16 +52,7 @@ public struct IdentifiedEnumCasesMacro: MemberMacro {
       let enumID = "enum ID: String, Equatable, CaseIterable {\n\(caseIds.map { "  case \($0)\n" }.joined())}"
       let idAccessor = "var id: ID {\n  switch self {\n\(caseIds.map { "  case .\($0): .\($0)\n" }.joined())  }\n}"
       
-      
-      let isPublic = if let modifiers = declaration.modifiers, !modifiers.children(viewMode: .fixedUp).filter({ syntax in
-        syntax.tokens(viewMode: .fixedUp).contains(TokenSyntax.keyword(.public)) }).isEmpty {
-        true
-      } else {
-        false
-      }
-      
-      
-      return if isPublic {
+      return if declaration.isPublic {
         [
           DeclSyntax(stringLiteral: "public \(enumID)"),
           DeclSyntax(stringLiteral: "public \(idAccessor)")
@@ -134,6 +83,29 @@ public struct IdentifiedEnumCasesMacro: MemberMacro {
     }
     
     public var severity: DiagnosticSeverity { .error }
+  }
+}
+
+private extension DeclGroupSyntax {
+  var isPublic: Bool {
+    let keywords: [Keyword] = {
+      if let modifiers = self.modifiers {
+        return modifiers.children(viewMode: .fixedUp).flatMap { syntax in
+          return syntax.as(DeclModifierSyntax.self)?.children(viewMode: .fixedUp).compactMap({ syntax in
+            switch syntax.as(TokenSyntax.self)?.tokenKind {
+            case .keyword(.public):
+              return Keyword.public
+            default:
+              return nil
+            }
+          }) ?? []
+        }
+      } else {
+        return []
+      }
+    }()
+    
+    return keywords.contains(Keyword.public)
   }
 }
 
